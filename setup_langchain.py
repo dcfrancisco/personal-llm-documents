71900037d216e87eb1962617b8a13f9c1af4f035
danny francisco <dcfran@gmail.com>
2023-07-01 01:23:24 +0800

    remove print

--- setup_langchain.py
+++ setup_langchain.py
@@ -7,14 +7,19 @@ from langchain.document_loaders import TextLoader
 from langchain.document_loaders import DirectoryLoader
 
 load_dotenv()
+
 # Set OpenAI API key
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+openAiApiKey = os.getenv("OPENAI_API_KEY")
 
 # Load and process the text files
 # loader = DocumentLoader('path/to/your/pdf/file.pdf')
-loader = DirectoryLoader("./new_articles/", glob="./*.pdf")
-# loader = DirectoryLoader("./new_articles/", glob="./*.txt", loader_cls=TextLoader)
-documents = loader.load()
+pdf_loader = DirectoryLoader("./new_articles/", glob="./*.pdf")
+txt_loader = DirectoryLoader("./new_articles/", glob="./*.txt", loader_cls=TextLoader)
+
+pdf_documents = pdf_loader.load()
+txt_documents = txt_loader.load()
+
+documents = pdf_documents + txt_documents
 
 # Splitting the text into chunks
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
